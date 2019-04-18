2054da80a0acf6bd44ab559cd2614ff61d078cd3
Israel Canul <icanul@iran-imac.royalresorts.local>
2019-04-18 17:58:31 -0500

    adding async action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,13 @@ import jsonPlaceHolder from "../api/jsonPlaceHolder";
 export const FETCH_USER = "FETCH_USER";
 
 // Action Creator
-export const fetchUser = () => {
-  // const response = await jsonPlaceHolder.get(
-  //   process.env.ROOT_API_JSONPLACEHOLDER + "/users/2"
-  // );
+export const fetchUser = async () => {
+  const response = await jsonPlaceHolder.get(
+    process.env.ROOT_API_JSONPLACEHOLDER + "/users/2"
+  );
   //Return an action
   return {
     type: FETCH_USER,
-    payload: null
+    payload: response
   };
 };
